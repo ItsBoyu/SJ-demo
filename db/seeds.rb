7eb8b6fbd4465009f9a5e7dcd3d016dbee7b9ec2
Boyu <rider945@outlook.com>
2020-06-30 21:33:33 +0800

    feature2: create menus and dishes by seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Menu.create(name: 'Menu1')
+Menu.create(name: 'Menu2')
+m1 = Menu.find_by(name: 'Menu1')
+m2 = Menu.find_by(name: 'Menu2')
+m1.dishes.create(name: 'dish1', price: 180)
+m1.dishes.create(name: 'dish2', price: 180)
+m1.dishes.create(name: 'dish3', price: 180)
+m2.dishes.create(name: 'dish4', price: 180)
+m2.dishes.create(name: 'dish5', price: 180)
+m2.dishes.create(name: 'dish6', price: 180)
