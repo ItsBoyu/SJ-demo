de66ca9c7565b6d29b3f28565064378d62a2d039
Boyu <rider945@outlook.com>
2020-06-30 21:41:25 +0800

    feature3: add route and controller (search) render json

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,4 +6,10 @@ class MenusController < ApplicationController
   def show
     @menu = Menu.find(params[:id])
   end
+
+  def search
+    menu = Menu.all
+  
+    render json: menu.to_json(include: [:dishes])
+  end
 end
