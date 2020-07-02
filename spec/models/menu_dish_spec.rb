3f3b8a04ef8ec525884782adb61babfa396a6a8e
Boyu <rider945@outlook.com>
2020-07-02 19:59:04 +0800

    expand RSpec model spec

--- spec/models/menu_dish_spec.rb
+++ spec/models/menu_dish_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MenuDish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { should belong_to(:dish)}
+    it { should belong_to(:menu) }
+  end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  context 'enum' do
+    it do
+      should define_enum_for(:period).
+        with_values([:lunch, :dinner])
+    end
+  end
+
+  context 'associations' do
+    it { should have_many(:menu_dishes)}
+    it { should have_many(:dishes).through(:menu_dishes) }
+  end
 end
